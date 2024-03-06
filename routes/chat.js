a50f4117517c269ef6db62ace27835f4042984fe
Louie <foxx999@hotmail.fr>
2024-03-06 16:26:59 +0100

    correct route

--- routes/chat.js
+++ routes/chat.js
@@ -1,37 +1,37 @@
-// var express = require("express");
-// var router = express.Router();
+var express = require("express");
+var router = express.Router();
 
-// const Pusher = require("pusher");
+const Pusher = require("pusher");
 
-// const pusherConfig = require("../pusher.json"); // (1)
-// const pusherClient = new Pusher(pusherConfig);
+const pusherConfig = require("../pusher.json"); // (1)
+const pusherClient = new Pusher(pusherConfig);
 
-// router.put("/:name", function (req, res) {
-//   // (3)
-//   console.log("User joined: " + req.params.name);
-//   pusherClient.trigger("chat_channel", "join", {
-//     name: req.params.name,
-//   });
-//   res.sendStatus(204);
-// });
+router.put("/:name", function (req, res) {
+  // (3)
+  console.log("User joined: " + req.params.name);
+  pusherClient.trigger("chat_channel", "join", {
+    name: req.params.name,
+  });
+  res.sendStatus(204);
+});
 
-// router.delete("/:name", function (req, res) {
-//   // (4)
-//   console.log("User left: " + req.params.name);
-//   pusherClient.trigger("chat_channel", "part", {
-//     name: req.params.name,
-//   });
-//   res.sendStatus(204);
-// });
+router.delete("/:name", function (req, res) {
+  // (4)
+  console.log("User left: " + req.params.name);
+  pusherClient.trigger("chat_channel", "part", {
+    name: req.params.name,
+  });
+  res.sendStatus(204);
+});
 
-// router.post("/:name/messages", function (req, res) {
-//   // (5)
-//   console.log("User " + req.params.name + " sent message: " + req.body.message);
-//   pusherClient.trigger("chat_channel", "message", {
-//     name: req.params.name,
-//     message: req.body.message,
-//   });
-//   res.sendStatus(204);
-// });
+router.post("/:name/messages", function (req, res) {
+  // (5)
+  console.log("User " + req.params.name + " sent message: " + req.body.message);
+  pusherClient.trigger("chat_channel", "message", {
+    name: req.params.name,
+    message: req.body.message,
+  });
+  res.sendStatus(204);
+});
 
-// module.exports = router;
+module.exports = router;
